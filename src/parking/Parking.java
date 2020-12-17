9920f491346dae3c0f4c3ed1945faff5eb097c75
HossamTourrato <61421680+HossamTourrato@users.noreply.github.com>
2020-12-17 13:46:20 +0200

    حبايبييي

--- src/parking/Parking.java
+++ src/parking/Parking.java
@@ -6,6 +6,7 @@ public class Parking {
     public static void main(String[] args) {
         System.out.println("Abdullah in");
         System.out.println("Hossam in");
+        System.out.println("سامووو عليكوو");
     }
     
 }
