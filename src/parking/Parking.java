22b9ff2d02d2d74653e992fd23ce6d2fa7964f2a
abdalabody811@gmail.com <abdalabody811@gmail.com>
2020-12-17 13:50:05 +0200

    done

--- src/parking/Parking.java
+++ src/parking/Parking.java
@@ -6,7 +6,6 @@ public class Parking {
     public static void main(String[] args) {
         System.out.println("Abdullah in");
         System.out.println("Hossam in");
-        System.out.println("سامووو عليكوو");
     }
     
 }
