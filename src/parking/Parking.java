e049ca715e23be95249200a6d109c1fe8171c2c4
HossamTourrato <61421680+HossamTourrato@users.noreply.github.com>
2020-12-17 13:44:07 +0200

    updated

--- src/parking/Parking.java
+++ src/parking/Parking.java
@@ -4,8 +4,8 @@ package parking;
 public class Parking {
 
     public static void main(String[] args) {
-        System.out.println("New github");
-        System.out.println("Newhub");
+        System.out.println("Abdullah in");
+        System.out.println("Hossam in");
     }
     
 }
